cadd6ea653ebe5c221ee21865e7edaf9791ece26
JBrean <jbrean@hotmail.co.uk>
2017-02-28 16:57:44 +0000

    End of first python session

--- basicpython.py
+++ basicpython.py
@@ -1,2 +1,10 @@
-print Hello world!
-print 11%2
+mylist = [23,"hi",2.4e-10]
+count = 0
+while count < 3:
+	item = mylist[count]
+	print item,mylist.index(item)
+	count += 1
+
+x = 0
+if x:
+	print x, " is true"
